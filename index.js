3791b68479bf26affa2717fb1a8949bf6fd43f2c
tania <taniaosano15@gmail.com>
2022-08-30 16:44:44 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,6 +1,12 @@
+//const { response } = require("express");
+
 function fetchBooks() {
   // To pass the tests, don't forget to return your fetch!
-  
+
+  return fetch('https://anapioficeandfire.com/api/books')
+  .then((resp)=>resp.json())
+  .then((json)=> renderBooks(json) );
+
 }
 
 function renderBooks(books) {
